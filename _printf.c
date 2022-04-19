20a692f74ee55cd649746c43241c5cdcfbdf812a
abrhamgg <abrhamgg@gmail.com>
2022-04-19 13:34:51 +0300

    fixed some bugs

--- _printf.c
+++ _printf.c
@@ -1,46 +1,47 @@
-#include <unistd.h>
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _printf - prints string, char, octal, hexadecmal values to the
- * stdout.
- * @format: the initial string that tell us what is going to be printed.
- * @...: amount of arguments.
- * Return: number of chars printed.
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
-
 int _printf(const char *format, ...)
 {
-	int i, count;
-	int (*f)(va_list);
-	va_list list;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (format == NULL)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(list, format);
-	i = 0;
-
-	while (format[i] != '\0')
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (format[i] == '%')
+		if (*p == '%')
 		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			f = get_func(format[i + 1]);
-			if (f == NULL)
-				count += print_nan(format[i], format[i + 1]);
-			else
-				count += f(list);
-			i++;
-		}
-		else
-		{
-			_putchar(format[i]);
-			count++;
-		}
-		i++;
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	va_end(list);
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
 }
--- main.h
+++ main.h
@@ -1,42 +1,76 @@
-#ifndef MAIN
-#define MAIN
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdarg.h>
 
 /**
- * struct ops - a structure containing a char to compare with formatb modifiers
- * and then choose the right function when it matches
- * @operation: the char we want to compare to
- * @func: the address of the function we want to return if a char matches
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
  */
-
-typedef struct ops
+typedef struct flags
 {
-	char operation;
-	int (*func)(va_list);
-} ops_f;
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
 int _printf(const char *format, ...);
-int _putchar(char c);
-int (*get_func(char ch))(va_list);
-int print_nan(char ch1, char ch2);
-int print_string(va_list list);
-int print_char(va_list list);
-int print_num(va_list list);
-void my_reverse(char str[], int len);
-char* my_itoa(int num, char* str, int base);
-int print_binary(va_list list);
-unsigned int base_len(unsigned int num, int base);
-int print_unsigned(va_list list);
-char *hex_conv(int num);
-char *rev_string(char *s);
-int print_hex(va_list list);
-int print_Hex(va_list list);
-int print_octal(va_list list);
-int print_S(va_list list);
-int print_p(va_list list);
-int print_r(va_list list);
-int print_rot13(va_list list);
 
-#endif
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
+
+#endif /* MAIN_H */
